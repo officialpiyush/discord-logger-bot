bb08209bda9e8b1d3b4fef9794282cb8ff1a2e40
Piyush <bhangalepiyush@gmail.com>
2019-04-28 16:05:18 +0530

    Im Done For Today!

--- src/index.ts
+++ src/index.ts
@@ -48,36 +48,38 @@ let setCommand = bot.registerCommand("set", (msg: Message) => {
     });
 
 setCommand.registerSubcommand("vclog", async (msg: Message): Promise<any> => {
-    console.log(msg.channelMentions);
+    console.log((msg.channelMentions as Array<string>).length);
     // if(typeof msg.channel !== TextChannel) throw bot.createMessage(msg.channel.id, "This Command can Only be Used In A Guild!")
     let guild = (msg.channel as TextChannel).guild;
     if (!guild) return bot.createMessage(msg.channel.id, "This Command can Only be Used In A Guild!")
     let channel: string;
-    let shouldSet: Boolean = true;
+    let shouldSet = true;
     if ((msg.channelMentions as Array<string>).length <= 0) shouldSet = false;
     channel = (msg.channelMentions as Array<string>)[0];
+    if(shouldSet){
     let gChannel = await guild.channels.get(channel);
     if (!gChannel) return bot.createMessage(msg.channel.id, "Channel Couldnt be Found!");
-    console.log()
+    }
+    console.log(shouldSet);
+
+
     switch (shouldSet) {
         case true:
         db.findOne({serverID: guild.id} , async (err, file) => {
             if (err) return console.log(chalk.red(err.stack));
             if(!file || file === null) {
-            let stage = new db({
-                serverID: guild.id, logging:
-                {
-                    voicelog: channel
-                }
-            });
+                console.log("0");
+            let stage = new db({serverID: guild.id, logging : { voicelog: channel}});
             await stage.save();
         } else if(file || file !== null){
+            console.log("1");
             await db.updateOne({serverID: guild.id} , {logging : { voicelog: channel}});
         }
     });
             break;
         
         case false:
+        console.log("2");
             await db.updateOne({serverID: guild.id} , {logging : { voicelog: undefined}});
             break;
     }
