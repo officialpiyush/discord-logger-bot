5a22ab30d8adef6775ad2ddfb46d676e30c4cfd6
Piyush <bhangalepiyush@gmail.com>
2019-07-01 20:22:26 +0530

    Somethings

--- src/events/channelUpdate.ts
+++ src/events/channelUpdate.ts
@@ -1,6 +1,8 @@
-import { CommandClient, TextChannel, VoiceChannel, CategoryChannel } from "eris";
+import { CommandClient, TextChannel, VoiceChannel, CategoryChannel,GuildChannel } from "eris";
 import { oldChannel } from "../interfaces/oldChannel";
 
 export = (_bot: CommandClient, channel: TextChannel | VoiceChannel | CategoryChannel, oldChannel: oldChannel) => {
-    
+    if(channel instanceof GuildChannel) {
+      
+    }
 }
